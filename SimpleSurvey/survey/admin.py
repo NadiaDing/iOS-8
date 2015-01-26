6aa3595eaa601aa947d5ab7f6131a0c5081ff50d
Nadia <nadja.ding@gmail.com>
2015-01-25 23:14:51 -0600

    iOS 8 Apps
    use Swift to write iOS apps

--- SimpleSurvey/survey/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from survey.models import Survey, Question, Choice, SurveyAnswer, QuestionAnswer
-# Register your models here.
-
-admin.site.register(Survey)
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(SurveyAnswer)
-admin.site.register(QuestionAnswer)
--- simple_blog/blogapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from blogapp.models import BlogArticle
-# Register your models here.
-
-admin.site.register(BlogArticle)
